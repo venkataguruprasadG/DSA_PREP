d60d468a651048145b8f385cd7e631461e12a542
venkataguruprasadG <venkatagp27@gmail.com>
2026-08-11 11:50:00 +0530

    largest of n numbers.

---
+++ largest.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n; // how many numbers
+
+    int num;
+    int result = num;
+
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> num;
+        if (num > result)
+        {
+            result = num;
+        }
+    }
+
+    cout << result << endl;
+
+    return 0;
+}
